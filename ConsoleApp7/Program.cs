2cd6cadcdacc13311ad5aa8300d2cdcadc8a12b4
doveka <131176206+doveka@users.noreply.github.com>
2023-12-27 04:59:26 +0200

    Add files via upload

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число A: ");
+        int A = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите число B: ");
+        int B = int.Parse(Console.ReadLine());
+
+        int N = 0;
+
+        Console.WriteLine("Числа, расположенные между A и B:");
+
+        for (int i = A; i <= B; i++)
+        {
+            Console.Write(i + " ");
+            N++;
+        }
+
+        Console.WriteLine("\nКоличество чисел: " + N);
+    }
+}
